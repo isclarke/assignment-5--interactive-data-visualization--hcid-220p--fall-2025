442b73e838a324b552e116307b0d81c4ec3ab598
isclarke <isclarke@hartford.edu>
2025-12-17 02:55:38 -0500

    Update main.js

--- 6.3--pyramid/main.js
+++ 6.3--pyramid/main.js
@@ -1,12 +1,15 @@
 const drawSalaryPyramid = (dataset) => {
 
-    const spacing = { top: 40, right: 30, bottom: 40, left: 60 };
+    const spacing = { top: 40, right: 30, bottom: 40, left: 80 };
     const svgWidth = 560;
     const svgHeight = 500;
 
     const chartWidth = svgWidth - spacing.left - spacing.right;
     const chartHeight = svgHeight - spacing.top - spacing.bottom;
 
+    /* ===================== */
+    /*   SVG + CHART GROUP   */
+    /* ===================== */
     const svg = d3.select(`#pyramid`)
         .append(`svg`)
         .attr(`viewBox`, `0 0 ${svgWidth} ${svgHeight}`);
@@ -14,10 +17,15 @@ const drawSalaryPyramid = (dataset) => {
     const chart = svg.append(`g`)
         .attr(`transform`, `translate(${spacing.left}, ${spacing.top})`);
 
+    /* ===================== */
+    /*      DATA SPLIT       */
+    /* ===================== */
     const femaleData = dataset.filter(d => d.gender === `Female`);
     const maleData = dataset.filter(d => d.gender === `Male`);
 
-
+    /* ===================== */
+    /*        BINNING        */
+    /* ===================== */
     const binGenerator = d3.bin()
         .value(d => d.salary)
         .domain([0, 240000])
@@ -26,6 +34,9 @@ const drawSalaryPyramid = (dataset) => {
     const femaleBins = binGenerator(femaleData);
     const maleBins = binGenerator(maleData);
 
+    /* ===================== */
+    /*        SCALES         */
+    /* ===================== */
     const maxPercent = 15;
 
     const leftScale = d3.scaleLinear()
@@ -36,12 +47,17 @@ const drawSalaryPyramid = (dataset) => {
         .domain([0, maxPercent])
         .range([chartWidth / 2, chartWidth]);
 
-    const yScale = d3.scaleLinear()
-        .domain([0, 240000])
-        .range([chartHeight, 0]);
+    const yScale = d3.scaleBand()
+        .domain(femaleBins.map(d => d.x0))
+        .range([chartHeight, 0])
+        .padding(0.1);
 
+    /* ===================== */
+    /*         BARS          */
+    /* ===================== */
     const barGroup = chart.append(`g`);
 
+    // FEMALE (LEFT)
     barGroup.selectAll(`.female-bar`)
         .data(femaleBins)
         .join(`rect`)
@@ -49,52 +65,60 @@ const drawSalaryPyramid = (dataset) => {
         .attr(`x`, d =>
             leftScale((d.length / femaleData.length) * 100)
         )
-        .attr(`y`, d => yScale(d.x1))
+        .attr(`y`, d => yScale(d.x0))
         .attr(`width`, d =>
             chartWidth / 2 -
-      leftScale((d.length / femaleData.length) * 100)
+            leftScale((d.length / femaleData.length) * 100)
         )
-        .attr(`height`, d =>
-            yScale(d.x0) - yScale(d.x1)
-        );
+        .attr(`height`, yScale.bandwidth());
 
+    // MALE (RIGHT)
     barGroup.selectAll(`.male-bar`)
         .data(maleBins)
         .join(`rect`)
         .attr(`class`, `male-bar`)
         .attr(`x`, chartWidth / 2)
-        .attr(`y`, d => yScale(d.x1))
+        .attr(`y`, d => yScale(d.x0))
         .attr(`width`, d =>
             rightScale((d.length / maleData.length) * 100) -
-      chartWidth / 2
+            chartWidth / 2
         )
-        .attr(`height`, d =>
-            yScale(d.x0) - yScale(d.x1)
-        );
+        .attr(`height`, yScale.bandwidth());
 
+    /* ===================== */
+    /*         AXES          */
+    /* ===================== */
+
+    // LEFT X AXIS
     chart.append(`g`)
         .attr(`transform`, `translate(0, ${chartHeight})`)
         .call(
             d3.axisBottom(leftScale)
                 .tickValues([15, 10, 5, 0])
+                .tickFormat(d => `${d}%`)
                 .tickSizeOuter(0)
         );
 
+    // RIGHT X AXIS
     chart.append(`g`)
         .attr(`transform`, `translate(0, ${chartHeight})`)
         .call(
             d3.axisBottom(rightScale)
                 .tickValues([5, 10, 15])
+                .tickFormat(d => `${d}%`)
                 .tickSizeOuter(0)
         );
 
+    // Y AXIS
     chart.append(`g`)
         .call(
             d3.axisLeft(yScale)
-                .tickValues(d3.range(0, 240001, 20000))
-                .tickFormat(d3.format(`,`))
+                .tickFormat(d => `$${d / 1000}k`)
         );
 
+    /* ===================== */
+    /*        LABELS         */
+    /* ===================== */
     svg.append(`text`)
         .attr(`x`, spacing.left + chartWidth / 2)
         .attr(`y`, svgHeight - 5)
@@ -107,10 +131,13 @@ const drawSalaryPyramid = (dataset) => {
         .text(`Yearly Salary (USD)`);
 };
 
+/* ===================== */
+/*     LOAD THE DATA     */
+/* ===================== */
 d3.csv(`/data/data.csv`, d => ({
     gender: d.gender,
     salary: +d.salary
 }))
-.then(data => {
-    drawSalaryPyramid(data);
-});
+    .then(data => {
+        drawSalaryPyramid(data);
+    });
