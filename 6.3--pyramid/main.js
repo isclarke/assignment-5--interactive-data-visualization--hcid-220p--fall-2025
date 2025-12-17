b5bf9291be6dbc1953908ab806b5c4e5d27de492
isclarke <isclarke@hartford.edu>
2025-12-17 02:34:53 -0500

    add css, and main.js

--- 6.3--pyramid/main.js
+++ 6.3--pyramid/main.js
@@ -1,100 +1,116 @@
 const drawSalaryPyramid = (dataset) => {
 
-  const spacing = { top: 40, right: 30, bottom: 40, left: 60 };
-  const svgWidth = 560;
-  const svgHeight = 500;
+    const spacing = { top: 40, right: 30, bottom: 40, left: 60 };
+    const svgWidth = 560;
+    const svgHeight = 500;
 
-  const chartWidth = svgWidth - spacing.left - spacing.right;
-  const chartHeight = svgHeight - spacing.top - spacing.bottom;
+    const chartWidth = svgWidth - spacing.left - spacing.right;
+    const chartHeight = svgHeight - spacing.top - spacing.bottom;
 
-  const svg = d3.select("#pyramid")
-    .append("svg")
-    .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`);
+    const svg = d3.select(`#pyramid`)
+        .append(`svg`)
+        .attr(`viewBox`, `0 0 ${svgWidth} ${svgHeight}`);
 
-  const chart = svg.append("g")
-    .attr("transform", `translate(${spacing.left}, ${spacing.top})`);
+    const chart = svg.append(`g`)
+        .attr(`transform`, `translate(${spacing.left}, ${spacing.top})`);
 
-  const femaleData = dataset.filter(d => d.gender === "Female");
-  const maleData = dataset.filter(d => d.gender === "Male");
+    const femaleData = dataset.filter(d => d.gender === `Female`);
+    const maleData = dataset.filter(d => d.gender === `Male`);
 
-  const binMaker = d3.bin()
-    .value(d => d.salary);
 
-  const femaleBins = binMaker(femaleData);
-  const maleBins = binMaker(maleData);
+    const binGenerator = d3.bin()
+        .value(d => d.salary)
+        .domain([0, 240000])
+        .thresholds(d3.range(0, 240001, 20000));
 
-  const maxPercent = 15;
+    const femaleBins = binGenerator(femaleData);
+    const maleBins = binGenerator(maleData);
 
-  const leftScale = d3.scaleLinear()
-    .domain([maxPercent, 0])
-    .range([0, chartWidth / 2]);
+    const maxPercent = 15;
 
-  const rightScale = d3.scaleLinear()
-    .domain([0, maxPercent])
-    .range([chartWidth / 2, chartWidth]);
+    const leftScale = d3.scaleLinear()
+        .domain([maxPercent, 0])
+        .range([0, chartWidth / 2]);
 
-  const salaryMin = femaleBins[0].x0;
-  const salaryMax = femaleBins[femaleBins.length - 1].x1;
+    const rightScale = d3.scaleLinear()
+        .domain([0, maxPercent])
+        .range([chartWidth / 2, chartWidth]);
 
-  const verticalScale = d3.scaleLinear()
-    .domain([salaryMin, salaryMax])
-    .range([chartHeight, 0]);
+    const yScale = d3.scaleLinear()
+        .domain([0, 240000])
+        .range([chartHeight, 0]);
 
-  const bars = chart.append("g");
+    const barGroup = chart.append(`g`);
 
-  bars.selectAll(".female-bar")
-    .data(femaleBins)
-    .join("rect")
-    .attr("class", "female-bar")
-    .attr("x", d => leftScale((d.length / dataset.length) * 100))
-    .attr("y", d => verticalScale(d.x1))
-    .attr("width", d =>
-      chartWidth / 2 - leftScale((d.length / dataset.length) * 100)
-    )
-    .attr("height", d =>
-      verticalScale(d.x0) - verticalScale(d.x1)
-    );
+    barGroup.selectAll(`.female-bar`)
+        .data(femaleBins)
+        .join(`rect`)
+        .attr(`class`, `female-bar`)
+        .attr(`x`, d =>
+            leftScale((d.length / femaleData.length) * 100)
+        )
+        .attr(`y`, d => yScale(d.x1))
+        .attr(`width`, d =>
+            chartWidth / 2 -
+      leftScale((d.length / femaleData.length) * 100)
+        )
+        .attr(`height`, d =>
+            yScale(d.x0) - yScale(d.x1)
+        );
 
-  bars.selectAll(".male-bar")
-    .data(maleBins)
-    .join("rect")
-    .attr("class", "male-bar")
-    .attr("x", chartWidth / 2)
-    .attr("y", d => verticalScale(d.x1))
-    .attr("width", d =>
-      rightScale((d.length / dataset.length) * 100) - chartWidth / 2
-    )
-    .attr("height", d =>
-      verticalScale(d.x0) - verticalScale(d.x1)
-    );
+    barGroup.selectAll(`.male-bar`)
+        .data(maleBins)
+        .join(`rect`)
+        .attr(`class`, `male-bar`)
+        .attr(`x`, chartWidth / 2)
+        .attr(`y`, d => yScale(d.x1))
+        .attr(`width`, d =>
+            rightScale((d.length / maleData.length) * 100) -
+      chartWidth / 2
+        )
+        .attr(`height`, d =>
+            yScale(d.x0) - yScale(d.x1)
+        );
 
-  chart.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(
-      d3.axisBottom(leftScale)
-        .tickValues([15, 10, 5, 0])
-        .tickSizeOuter(0)
-    );
+    chart.append(`g`)
+        .attr(`transform`, `translate(0, ${chartHeight})`)
+        .call(
+            d3.axisBottom(leftScale)
+                .tickValues([15, 10, 5, 0])
+                .tickSizeOuter(0)
+        );
 
-  chart.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(
-      d3.axisBottom(rightScale)
-        .tickValues([5, 10, 15])
-        .tickSizeOuter(0)
-    );
+    chart.append(`g`)
+        .attr(`transform`, `translate(0, ${chartHeight})`)
+        .call(
+            d3.axisBottom(rightScale)
+                .tickValues([5, 10, 15])
+                .tickSizeOuter(0)
+        );
 
-  chart.append("g")
-    .call(d3.axisLeft(verticalScale));
+    chart.append(`g`)
+        .call(
+            d3.axisLeft(yScale)
+                .tickValues(d3.range(0, 240001, 20000))
+                .tickFormat(d3.format(`,`))
+        );
 
-  svg.append("text")
-    .attr("x", spacing.left + chartWidth / 2)
-    .attr("y", svgHeight - 5)
-    .attr("text-anchor", "middle")
-    .text("Percentage of respondents");
+    svg.append(`text`)
+        .attr(`x`, spacing.left + chartWidth / 2)
+        .attr(`y`, svgHeight - 5)
+        .attr(`text-anchor`, `middle`)
+        .text(`Percent of respondents`);
 
-  svg.append("text")
-    .attr("x", 5)
-    .attr("y", 20)
-    .text("Annual Salary (USD)");
+    svg.append(`text`)
+        .attr(`x`, 5)
+        .attr(`y`, 20)
+        .text(`Yearly Salary (USD)`);
 };
+
+d3.csv(`/data/data.csv`, d => ({
+    gender: d.gender,
+    salary: +d.salary
+}))
+.then(data => {
+    drawSalaryPyramid(data);
+});
